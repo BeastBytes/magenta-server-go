90613cbd5bec6d5f8b9747c228aa43c4c88c5ed1
Jeramy Singleton <jeramyRR@gmail.com>
2015-04-12 18:17:40 -0500

    Add Client object.  Client is currently made up of a single struct with a single string field, Name

---
+++ client.go
@@ -0,0 +1,16 @@
+/************************************************
+* client.go
+* Author: Jeramy Singleton
+* Date: 12 April 2015
+*
+* Description:  A client is a remote user that
+* has connected to the server.  Information for
+* the client should be stored in a struct and
+* be accessible to the server.
+*************************************************/
+
+package main
+
+type Client struct {
+	Name string
+}
