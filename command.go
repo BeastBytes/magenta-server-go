78e97545c2e6ef9c6cd35d6422e2105037b414d3
Jeramy Singleton <jeramyRR@gmail.com>
2015-05-25 11:26:46 -0500

    Add server parameter to NewCommand function

--- command.go
+++ command.go
@@ -2,10 +2,10 @@ package main
 
 type Command struct {
 	Name string
-	Cmd  func(c *Client, words []string)
+	Cmd  func(c *Client, s *Server, words []string)
 }
 
 // NewCommand returns a new Command
-func NewCommand(name string, cmd func(c *Client, words []string)) *Command {
+func NewCommand(name string, cmd func(c *Client, s *Server, words []string)) *Command {
 	return &Command{Name: name, Cmd: cmd}
 }
